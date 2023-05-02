53b5de0d7214c0e85f745ed2065fd44109b247de
shoutingsharma <akashkofficial54@gmail.com>
2023-05-02 15:38:50 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Category</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Product</div>,
+  },
+]);
 
 const App = () => {
   return (
     <div>
-      App
+    <RouterProvider router={router} />
     </div>
   )
 }
